207c2ec88f4d0057a738140509f10b19d7ff7668
Quan Dang <37484547+quandang23@users.noreply.github.com>
2022-12-22 13:02:15 -0600

    Add files via upload

--- irReceiver1.2.ino
+++ irReceiver1.2.ino
@@ -19,6 +19,8 @@ const int redLED = 23;
 const int greenLED = 22; 
 const int sensor = 21; 
 int stage = 0; 
+int reset = 0; 
+int start = 0; 
 
 // Current value of output characteristic persisted here
 static uint8_t outputData[1];
@@ -43,9 +45,11 @@ class InputReceivedCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharWriteState) {
         uint8_t *inputValues = pCharWriteState->getData();
         if(*inputValues == 0x01){
+          start = 1; 
           Serial.println("timerStart"); 
         }
         else if (*inputValues == 0x02){
+          reset = 1; 
           Serial.println("timerReset");
         }
 //        switch(inputValues[2]) {
@@ -118,24 +122,63 @@ void setup() {
   Serial.println("BLE Service is advertising");
 }
 //stage 0: no ble established, red led is off. 
-//stage 1: if sensor = 0, send a signal of 1. 
-void loop() {
-  int input = digitalRead(sensor); 
-  if (stage == 1 && input == 0){
-    digitalWrite(redLED, HIGH); 
-    outputData[0] = 1; 
-    pOutputChar->setValue((uint8_t *)outputData, 1);
-    pOutputChar->notify();
-    delay(1000);
-  }
-  else if (stage == 0){
+//stage 1: if sensor = 0, send a signal of 1.
+//reset 0: reset is not clicked
+//reset 1: reset is clicked 
+
+void loop(){
+  if (stage == 0)     //bluetooth is not established
+  {
     digitalWrite(redLED, LOW); 
     digitalWrite(greenLED, HIGH); 
     delay(300); 
     digitalWrite(greenLED, LOW); 
     delay(300); 
   }
-  else if (stage == 1){
+  while (stage == 1) //bluetooth is established 
+  {
     digitalWrite(greenLED, HIGH);
+    int input = digitalRead(sensor); 
+    if (start == 1 && input == 0)  //if start is pressed && crossing is detected then turn on the red LED and notify the app. 
+    {
+      outputData[0] = 1; 
+      pOutputChar->setValue((uint8_t *)outputData, 1);
+      pOutputChar->notify();
+      digitalWrite(redLED, HIGH); 
+      start = 0;     //reseting the start signal
+      delay(1000);
+    }
+    else if (reset == 1)   //if reset is clicked, then turn off the red LED
+    {
+      digitalWrite(redLED, LOW); 
+      reset = 0; 
+    }
   }
 }
+
+/*
+ * void loop() {
+  int input = digitalRead(sensor); 
+  if (stage == 1 && input == 0){  //when crossing is detected. 
+    digitalWrite(redLED, HIGH); 
+    outputData[0] = 1; 
+    pOutputChar->setValue((uint8_t *)outputData, 1);
+    pOutputChar->notify();
+    delay(1000);
+  }
+  else if (stage == 0){   //bluetooth is not established
+    digitalWrite(redLED, LOW); 
+    digitalWrite(greenLED, HIGH); 
+    delay(300); 
+    digitalWrite(greenLED, LOW); 
+    delay(300); 
+  }
+  else if (stage == 1){   //bluetooth is establishedd
+    digitalWrite(greenLED, HIGH);
+  }
+  if (reset == 1){  //reset is clicked
+    digitalWrite(redLED, LOW); 
+    reset = 0; 
+  }
+}
+*/
